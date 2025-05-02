b8bdc7afcc980b2efa3bdcd84cfc7220016d6c46
Yasmin Giraldo Castaño <98331961+Yasgc24@users.noreply.github.com>
2025-05-02 10:54:40 -0500

    Creacion de modelo para franquicia

---
+++ src/main/java/com/prueba/franquicia/model/Franquicia.java
@@ -0,0 +1,49 @@
+package com.prueba.franquicia.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Document(collection = "franquicias")
+public class Franquicia {
+
+    @Id
+    private String id;
+    private String nombre;
+    private List<Sucursal> sucursales = new ArrayList<>();
+
+    public Franquicia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Sucursal> getSucursales() {
+        return sucursales;
+    }
+
+    public void setSucursales(List<Sucursal> sucursales) {
+        this.sucursales = sucursales;
+    }
+
+    public void agregarSucursal(Sucursal sucursal) {
+        this.sucursales.add(sucursal);
+    }
+}
