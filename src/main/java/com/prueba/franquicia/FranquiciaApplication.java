3f95e6272c8bacab0efac6293ec627607d6f399e
Yasgc24 <yasmin.giraldo.castano@gmail.com>
2025-05-04 12:03:28 -0500

    Actualizar conexion a base de datos

--- src/main/java/com/prueba/franquicia/FranquiciaApplication.java
+++ src/main/java/com/prueba/franquicia/FranquiciaApplication.java
@@ -2,12 +2,17 @@ package com.prueba.franquicia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class FranquiciaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FranquiciaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("MONGODB_USER", dotenv.get("MONGODB_USER"));
+        System.setProperty("MONGODB_PASSWORD", dotenv.get("MONGODB_PASSWORD"));
+        System.setProperty("MONGODB_CLUSTER", dotenv.get("MONGODB_CLUSTER"));
 
+        SpringApplication.run(FranquiciaApplication.class, args);
+    }
 }
